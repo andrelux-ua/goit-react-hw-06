9cabe4ba9229a5e50d5f9028e3e4e2e36faede30
Andriy Kiselyov <168927164+andrelux-ua@users.noreply.github.com>
2025-04-05 18:53:51 +0300

    new-img

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,25 @@
 import css from './Layout.module.css';
+import img from '../../assets/images/s15.png';
 
 function Layout({ children }) {
   return (
     <main className={css.app}>
+      <img
+        src={img}
+        alt="telephone"
+        style={{
+          height: '712px',
+          width: '710px',
+          position: ' absolute;',
+          top: '11.6vh',
+          left: '43.4%',
+          transform: 'translateX(-50%)',
+          zIndex: '-1',
+          objectFit: 'cover',
+          filter: 'blur(100px)',
+          opacity: '0.3',
+        }}
+      />
       <div className={css.contentWrapper}>{children}</div>
     </main>
   );
